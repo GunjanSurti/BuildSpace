0a1d65b9845291a58f69fd533ebafcaf28f1eb60
GunjanSurti <gunjansurti142@gmail.com>
2023-01-30 23:19:42 +0530

    Inherited contracts
    Imported OpenZeppelin's ERC721.sol and Counter's Counter.sol contract
    also made makeAnEpicNFT function

--- Build your own NFT Collection/epic-nfts/contracts/MyEpicNFTs.sol
+++ Build your own NFT Collection/epic-nfts/contracts/MyEpicNFTs.sol
@@ -1,10 +1,45 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.17;
 
-import "hardhat/console.sol";
+// We first import some Openzeppelin Contracts
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyEpicNft {
-    constructor() {
+import "hardhat/console.sol";
+.
+// We inherit the contract we imported. This means we'll have access
+// to the inherited contract's methods.
+
+// ERC721 -> parent, MyEpicNft -> Child
+contract MyEpicNft is ERC721 {
+    // Magic given to us by OpenZeppelin to hepl us keep track of tokenIds
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // we need to pass the name of our NFTs token and its symbol.
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
         console.log("This is my Nft collection");
     }
+
+    //A function our user will hit to get their NFT.
+    function makeAnEpicNFT() public {
+        //Get the current tokenId, this strats at 0
+        uint256 newItemId = _tokenIds.current();
+
+        //Actually mint the NFT to the sender using msg.sender
+        _safeMint(msg.sender, newItemId);
+
+        //return the NFT's metadata
+        tokenURI(newItemId);
+
+        //Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
+
+        // set the NFT's metadata
+        function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+            require(_exists(_tokenId));
+            return "blah";
+        }
+    }
+
 }
