2aca5254e92df78c09519867284e83ab494dd928
GunjanSurti <gunjansurti142@gmail.com>
2023-01-30 19:27:01 +0530

    New Project => NFT Collection

---
+++ Build your own NFT Collection/epic-nfts/contracts/MyEpicNFTs.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract MyEpicNft {
+    constructor() {
+        console.log("This is my Nft collection");
+    }
+}
